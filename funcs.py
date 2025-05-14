37056396cbfcd2e35a86ef3297f73bf351e7d32c
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-05-14 15:40:41 -0300

    criado pasta de funções e v2.py para uma maneira mais clara de projeto

---
+++ funcs.py
@@ -0,0 +1,10 @@
+def urls():
+    {'main':'https://esi.evetech.net/ui/',
+     'regions_id':'https://esi.evetech.net/latest/universe/regions/',
+     'regions_name':'https://esi.evetech.net/latest/universe/regions/{region_id}/',
+     'constellations_id':'https://esi.evetech.net/latest/universe/constellations/',
+     'constellations_name':'https://esi.evetech.net/latest/universe/constellations/{constellation_id}/',
+     'systems_id':'https://esi.evetech.net/latest/universe/systems/',
+     'systems_name':'https://esi.evetech.net/latest/universe/systems/{system_id}/',
+     }
+    
