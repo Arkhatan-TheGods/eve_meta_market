8257188cd84717420a19d15f52db22e2e342e5bf
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-05-22 10:34:04 -0300

    parte 2

--- v2.py
+++ v2.py
@@ -1,14 +1,13 @@
 import requests, sqlite3, funcs
-#from funcs import fodeu
+
 if __name__ == "__main__":
     try:
         urls = funcs.urls()
-        if requests.get(urls['main']) == '200':
+        if requests.get(urls['main']) == 200:
             print('funcionou')
-        
         #main_url = 'https://esi.evetech.net/ui/'
         #conn = requests.get(main_url)
         #if conn.status_code == 200:
-        pass
+            
     except Exception as e:
         print(e)
