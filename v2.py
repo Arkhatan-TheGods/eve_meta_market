640453d2b50e98c97ea4ce6e7237b0548aef4030
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-05-28 13:59:15 -0300

    o que fazer.

--- v2.py
+++ v2.py
@@ -1,10 +1,10 @@
-import requests, sqlite3, funcs, dotenv
-
+import requests, sqlite3, funcs, os
+from dotenv import load_dotenv
 #o que fazer? verificar se existe um arquivo db, se deseja atualizar, 
 if __name__ == "__main__":
     try:
         urls = funcs.urls()
-        
+        #colocar a função que verifica se existe um db e se caso não existir criar um, baixar as ids
         if requests.get(urls['main']) == 200:
             pass
             
