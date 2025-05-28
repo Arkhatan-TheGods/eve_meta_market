b2416b7a5183bcbfe937da374cfa8bb98e54076d
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-05-28 20:48:11 -0300

    adição do dotenv no projeto

--- v2.py
+++ v2.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 #o que fazer? verificar se existe um arquivo db, se deseja atualizar, 
 if __name__ == "__main__":
     try:
+        config = load_dotenv('config.env')
         urls = funcs.urls()
+        '''if os.getenv('DB_ROUTE'):
+            print('sucesso')
+        else:
+            print('falha')'''
+        
         #colocar a função que verifica se existe um db e se caso não existir criar um, baixar as ids
         if requests.get(urls['main']) == 200:
             pass
