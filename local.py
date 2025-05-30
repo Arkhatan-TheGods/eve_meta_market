ede304dde2c21ed39ef492e3a8cae4e1ae624c38
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-05-29 22:35:35 -0300

    bem, não foi, depois procuro o porquê.

--- local.py
+++ local.py
@@ -6,12 +6,15 @@ if requests.get('https://esi.evetech.net/ui/').status_code == 200:
     regions = requests.get('https://esi.evetech.net/latest/universe/regions/')
     data = []
     for region in regions.json():
-        data.append((region,None))
+        data.append({region:None})
     #print(data)
     db_route = os.getenv('DB_ROUTE')
-    if os.path.exists(db_route):
-        conn = sqlite3.connect(db_route)
-        cursor = conn.cursor()
-        cursor.executemany('INSERT OR REPLACE INTO Regions(region_id, name) VALUES (?, ?)',data)
-        conn.commit()
-        conn.close()
+    #if os.path.exists(db_route):
+    conn = sqlite3.connect(db_route)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS Regions (region_id INTEGER, name text)')
+    cursor.executemany('INSERT INTO Regions(region_id, name) VALUES (:region_id ,:name)',data)
+    conn.commit()
+    conn.close()
+        #elif not os.path.exists(db_route):
+            #pass
