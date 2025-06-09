9d62b59d2b00b58b4f3e98c730d2170fe8a50dc0
Arkhatan-TheGods <pauloevertron@outlook.com>
2025-06-09 19:13:56 -0300

    objetivo

--- tests.py
+++ tests.py
@@ -9,10 +9,13 @@ for groups in conn.json():
     #print(data)
     #sleep(2)
 
-for id in data:
+for id in range(len(data)):
     position = data[id]
+    print(position)
     conn2 = requests.get(f'{base_url}{position}').json()
     print(conn2)
     sleep(5)
-
-"""IMPORTANTE: o id do Ship Equipment é 9"""
+    exit()
+"""IMPORTANTE: o id do Ship Equipment é 9
+arrumado o problema de que ele mandava o request já com id 5
+novo foco: pegar os nomes desse request"""
